e22ff29ab07a70aef64c1bcd0ad928b113494ba6
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2023-08-02 17:09:23 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,5 @@
-import './src/modal.js';
-import './src/mobile-menu.js';
+import './js/back-to-top.js'
+import './js/smooth-scroll.js'
+import './js/swiper.js'
+import './js/modal.js';
+import './js/mobile-menu.js';
